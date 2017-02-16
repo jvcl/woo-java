cec6fd5bc0c12ef5f39c2b0fa17a944b11de134f
Jorge Valdivia <valdi.jorge@gmail.com>
2017-02-16 23:23:20 +1030

    added license

--- src/main/java/com/flyonet/woo_api/Product.java
+++ src/main/java/com/flyonet/woo_api/Product.java
@@ -1,8 +1,25 @@
+/*
+ * This file is part of WOO-JAVA
+ *
+ * Copyright (C) 2017 Jorge Valdivia
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+
 package com.flyonet.woo_api;
 
-/**
- * Created by jorge on 16/2/17.
- */
 public class Product {
     private int id;
     private String title;
