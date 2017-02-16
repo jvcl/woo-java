cd87ec938c16e932b69adb997e490f183690b3eb
Jorge Valdivia <valdi.jorge@gmail.com>
2017-02-16 22:51:29 +1030

    initial commit

---
+++ src/main/java/com/flyonet/woo_api/Product.java
@@ -0,0 +1,43 @@
+package com.flyonet.woo_api;
+
+/**
+ * Created by jorge on 16/2/17.
+ */
+public class Product {
+    private int id;
+    private String title;
+    private float price;
+
+    Product(){
+
+    }
+    Product(int id, String title, float price) {
+        this.id = id;
+        this.title = title;
+        this.price = price;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public float getPrice() {
+        return price;
+    }
+
+    public void setPrice(float price) {
+        this.price = price;
+    }
+}
---
+++ src/main/java/com/flyonet/woo_api/WooCommerce.java
@@ -0,0 +1,72 @@
+package com.flyonet.woo_api;
+
+import java.net.URL;
+
+/**
+ * Created by jorge on 16/2/17.
+ */
+public class WooCommerce {
+    private String consumerKey;
+    private String consumerSecret;
+    private String url;
+    private final URL URL;
+
+    public WooCommerce(URL URL, String consumerKey, String consumerSecret) {
+        this.consumerKey = consumerKey;
+        this.consumerSecret = consumerSecret;
+        this.URL = URL;
+    }
+
+    public Product createProduct(){
+        return new Product();
+    }
+
+    public Product getProductByID(int ID) {
+        return new Product();
+
+    }
+    public Product getProductBySKU(String SKU) {
+        return new Product();
+    }
+
+    public Product updateProduct(Product product) {
+        return new Product();
+    }
+
+    public Product deleteProduct(Product product) {
+        int id = product.getId();
+        return new Product();
+    }
+
+    public Product deleteProduct(int ID) {
+        return new Product();
+    }
+
+    public String getConsumerKey() {
+        return consumerKey;
+    }
+
+    public void setConsumerKey(String consumerKey) {
+        this.consumerKey = consumerKey;
+    }
+
+    public String getConsumerSecret() {
+        return consumerSecret;
+    }
+
+    public void setConsumerSecret(String consumerSecret) {
+        this.consumerSecret = consumerSecret;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public java.net.URL getURL() {
+        return URL;
+    }
+}
